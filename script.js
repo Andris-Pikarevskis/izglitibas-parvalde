0919df10171170899f66103ce4f73e7a436169b0
Andris Pikarevskis <A.Pikarevskis@latinsoft.lv>
2020-11-20 16:21:33 +0200

    noteikumi

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('#menu-togle');
-const mobileNav = document.querySelector('#mobile-nav');
-
-menuToggle.onclick = function() {
-    menuToggle.classList.toggle('menu-icon-active');
-    mobileNav.classList.toggle('mobile-nav--active');
-}
