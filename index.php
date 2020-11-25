e1958c02f3d9aa1e085322ac313b6ac0742ed042
Raimonds Fridenbergs <R.Fridenbergs@latinsoft.lv>
2020-11-25 15:45:16 +0200

    php process

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <link rel="icon" type="image/png" sizes="32x32" href="Pictures/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="Pictures/favicon/favicon-16x16.png">
     <link rel="manifest" href="Pictures/favicon/site.webmanifest">
-    <link rel="stylesheet" type="text/css" href="reset.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
@@ -19,6 +19,7 @@
         <header class="header">
         </header>
 
+        <form action="action.php" method="POST">
         <div class="Rectangle1">
             <div class="sign-up">Daugavpils izglītības pārvaldes mājas lapu izvērtējums MK noteikumu nr: 445
                 atbilstībai.</div>
@@ -26,72 +27,66 @@
         <div class="information-blocks">
             <div class="information-fields">
                 <div class="Rectangle2">
-                    <h2 class="block-title">Piekļūstamības paziņojums</h2>
-                    <div class="ievades-lauli">
-                        <form class="input">
-                            <div class="name">
-                                <input type="text" name="" placeholder="iestādes nosaukums">
-                            </div>
-                            <div class="name">
-                                <input type="text" name="" placeholder="norādīt tīmekļvietnes adresi – URL">
-                            </div>
-                            <div class="name">
-                                <input type="text" name="" placeholder="izmantotā izvērtēšanas metode">
-                            </div>
-                            <div class="name">
-                                <input type="text" name="" placeholder="saite uz izvērtējumu apliecinošu protokolu">
-                            </div>
-                        </form>
-                    </div>
+                        <h2 class="block-title">Piekļūstamības paziņojums</h2>
+                        <div class="ievades-lauli">
+                                <div class="name">
+                                    <input type="text" name="name" placeholder="iestādes nosaukums">
+
+                                </div>
+                                <div class="name">
+                                    <input type="text" name="url" placeholder="norādīt tīmekļvietnes adresi – URL">
+                                </div>
+                                <div class="name">
+                                    <input type="text" name="method" placeholder="izmantotā izvērtēšanas metode">
+                                </div>
+                                <div class="name">
+                                    <input type="text" name="protocol_url" placeholder="saite uz izvērtējumu apliecinošu protokolu">
+                                </div>
+                        </div>
+
                 </div>
             </div>
             <div class="information-fields">
-                <div class="Rectangle2">
-                    <h2 class="block-title">Atbilstības status</h2><br>
+            <div class="Rectangle2">
+                    <h2 class="block-title">Atbilstības status</h2>
+                    <button type="button" class="add-more-fit-status">Add more (+)</button>
+                    <br>
                     <div class="text2">Atzīmēt vajadzīgo – atbilstoši izvēlētajam statusam:</div><br>
                     <div class="ievades-lauli">
-                        <form class="input1">
-
-                            <input type="checkbox" id="" name="" value="">
+                            <input type="checkbox" name="if_fit_status[]" value="1">
                             <label for="">pilnīgi atbilst </label><br>
-                            <input type="checkbox" id="" name="" vacheckbox">
+                            <input type="checkbox" name="if_fit_status[]" value="0">
                             <label for="">neatbilst </label><br>
-                            <input type="checkbox" id="" name="" value="">
+                            <input type="checkbox" name="if_fit_status[]" value="2">
                             <label for="">daļēji atbilst </label>
 
                             <div class="name">
-                                <input type="text" name="" placeholder="Tīmekļvietne">
-                                <textarea rows="15" cols="38"
-                                    placeholder="norādīt neatbilstošo saturu vai pamatot atbrīvojumu, norādot attiecīgos minēto noteikumu punktus"></textarea>
+                                <input type="text" name="fit_url[]" placeholder="Tīmekļvietne">
+                                <textarea rows="15" name="fit_reason[]"
+                                placeholder="norādīt neatbilstošo saturu vai pamatot atbrīvojumu, norādot attiecīgos minēto noteikumu punktus"></textarea>
                             </div>
-
-                        </form>
-
                     </div>
                 </div>
+            <div class="more-fit-status-container">
+            </div>
             </div>
             <div class="information-fields">
                 <div class="Rectangle2">
                     <h2 class="block-title">Nepiekļūstamais saturs</h2><br>
                     <div class="text2">Atzīmēt vajadzīgo – atbilstoši izvēlētajam statusam:</div><br>
                     <div class="ievades-lauli">
-                        <form class="input1">
-
-                            <input type="checkbox" id="" name="" value="">
+                            <input type="checkbox" name="if_inaccessible_content" value="0">
                             <label for="">Neatbilstība prasībām</label><br>
-                            <input type="checkbox" id="" name="" value="">
+                            <input type="checkbox" name="if_inaccessible_content" value="1">
                             <label for="">piekļūstamības prasību nodrošināšana rada nesamērīgu slogu</label><br>
-                            <input type="checkbox" id="" name="" value="">
+                            <input type="checkbox" name="if_inaccessible_content" value="2">
                             <label for="">Uz saturu neattiecas noteikumu prasības</label>
 
                             <div class="name">
-                                <input type="text" name="" placeholder="Tīmekļvietne">
-                                <textarea rows="15" cols="38"
+                                <input type="text" name="inaccessible_url" placeholder="Tīmekļvietne">
+                                <textarea rows="15" cols="38" name="inaccessible_reason"
                                     placeholder="norādīt tīmekļvietņu neatbilstības un norādīt, kuras sadaļas/saturs/funkcijas nav atbilstošas piekļūstamības prasībām."></textarea>
                             </div>
-
-                        </form>
-
                     </div>
                 </div>
             </div>
@@ -102,14 +97,10 @@
                 <h2 class="block-title">Piekļūstamības alternatīvas</h2><br>
                 <div class="text2">(atbilstoši atbilstības statusam un nepiekļūstamajam saturam)</div><br>
                 <div class="ievades-lauli">
-                    <form class="input1">
-
                         <div class="name">
-                            <textarea rows="4" cols="38"
+                            <textarea rows="4" cols="38" name="access_alternative"
                                 placeholder="norādīt tīmekļvietņu neatbilstības un norādīt, kuras sadaļas/saturs/funkcijas nav atbilstošas piekļūstamības prasībām."></textarea>
                         </div>
-
-                    </form>
                 </div>
 
             </div>
@@ -117,25 +108,30 @@
             <div class="down-block2">
                 <h2 class="block-title">Ziņas par paziņojuma sagatavošanu</h2><br><br>
                 <div class="ievades-lauli">
-                    <form class="input1">
-
                         <div class="name">
-                            <textarea rows="4" cols="38"
+                            <textarea rows="4" cols="38" name="note_first_date"
                                 placeholder="(norādīt PIRMREIZĒJĀ paziņojuma sagatavošanas datumu)"></textarea><br>
-                            <textarea rows="4" cols="38"
+                            <textarea rows="4" cols="38" name="note_remind_date"
                                 placeholder="(norādīt paziņojuma ATKRTOTĀ pārskatīšanas datumu; nenorāda, ja šis ir pirmreizējais paziņojums)"></textarea><br>
-
                         </div>
-
-                    </form>
-
                 </div>
             </div>
+            <input type="submit" name="submitForm" class="button" value="submit">
 
         </div>
-
+        
+        </form>
 
     </main>
+
 </body>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    $('.add-more-fit-status').click(function() {
+        $('.more-fit-status-container').append(
+            '<div class="Rectangle2"><div class="text2">Atzīmēt vajadzīgo – atbilstoši izvēlētajam statusam:</div><div class="ievades-lauli"><input type="checkbox" name="if_fit_status[]" value="1"><label for="">pilnīgi atbilst </label><br><input type="checkbox" name="if_fit_status[]" value="0"><label for="">neatbilst </label><br><input type="checkbox" name="if_fit_status[]" value="2"><label for="">daļēji atbilst </label><div class="name"><input type="text" name="fit_url[]" placeholder="Tīmekļvietne"><textarea rows="15" name="fit_reason[]" placeholder="norādīt neatbilstošo saturu vai pamatot atbrīvojumu, norādot attiecīgos minēto noteikumu punktus"></textarea></div></div></div>'
+        );
+    });
+</script>
 </html>
---
+++ output-form.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/output.css">
+</head>
+
+<?php
+
+include('inc/config.php');
+
+global $db;
+$id = ((int) $_GET['id'] > 0) ? (int) $_GET['id'] : 0;
+$query = sprintf("SELECT * FROM content WHERE id = %s", $id);
+$data = $db->rawQuery($query);
+
+if (empty($data[0]['content'])) {
+        exit('Wrong id' );
+}
+
+$data = json_decode($data[0]['content'], false);
+
+$str = '';
+for ($i = 0; $i < count($data->if_fit_status); $i++) {
+
+        if ($data->if_fit_status[$i] == 1 && !empty($data->fit_url[$i])) { //atbilst prasībām
+                $str .= sprintf('<p>Tīmekļvietne</p><br><p><input type="checkbox" id="" name="" value="" checked><strong> pilnīgi atbilst </strong>noteikumiem Nr: 445</p><p><input type="text" name="" placeholder="(var norādīt vairākas tīmekļvietnes un/vai mobilās lietotnes)" class="iestades-nosaukums1" value="%s"></p>', $data->fit_url[$i]);
+        } 
+        
+        if ($data->if_fit_status[$i] == 2 && (!empty($data->fit_url[$i])) || (!empty($data->fit_reason[$i]))) { //daļēji atbilst
+                $str .= (!empty($data->fit_url[$i])) ? sprintf('<p><input type="text" name="" placeholder="(var norādīt vairākas tīmekļvietnes un/vai mobilās lietotnes)" class="iestades-nosaukums1" value="%s"></p>', $data->fit_url[$i]) : '';
+                $str .= (!empty($data->fit_reason[$i])) ? sprintf('<p><input type="text" name="" placeholder="(norādīt neatbilstošo saturu vai pamatot atbrīvojumu, norādot attiecīgos minēto noteikumu punktus)" class="iestades-nosaukums1" value="%s"></p><br>',  $data->fit_reason[$i]) : '';
+        } 
+
+        if ($data->if_fit_status[$i] == 0  && (!empty($data->fit_url[$i])) || (!empty($data->fit_reason[$i]))) { //neatbilst prasībām
+                $str .= '<p><br />Tīmekļvietne</p><p><strong><input type="checkbox" name="" value="" checked> neatbilst </strong>noteikumiem Nr: 445 šādu neatbilstību/atbrīvojumu dēļ:</p>';
+                $str .= (!empty($data->fit_url[$i])) ? sprintf('<p> <input type="text" name="" placeholder="(var norādīt vairākas tīmekļvietnes)" class="iestades-nosaukums1" value="%s"></p>', $data->fit_url[$i]) : '';
+                $str .= (!empty($data->fit_reason[$i])) ? sprintf('<p><input type="text" name="" placeholder="(norādīt neatbilstošo saturu vai pamatot atbrīvojumu, norādot attiecīgos minēto noteikumu punktus)" class="iestades-nosaukums1" value="%s"></p>', $data->fit_reason[$i]) : '';
+        }
+}
+
+?>
+<body>
+        <h2>Piekļūstamības paziņojums</h2>
+        <p><input type="text" name="" placeholder="iestādes nosaukums" class="iestades-nosaukums" value="<?= $data->name; ?>"> saskaņā ar</p>
+        <p>Ministru kabineta 2020. gada 14. jūlija noteikumiem Nr. 445 "Kārtība, kādā iestādes ievieto informāciju internetā" (turpmāk - noteikumi Nr. 445) apņemas savu tīmekļvietni/<br>mobilo lietotni (pasvītrot vajadzīgo) veidot piekļūstamu.</p>
+        <p>Šis paziņojums attiecas uz: 
+        <input type="text" name=""
+            placeholder="(norādīt tīmekļvietnes/mobilās lietotnes adresi - URL; var būt vairākas)"
+            class="iestades-nosaukums" 
+            value="<?= $data->url; ?>"></p>
+
+        <p><br /> Izvērtējums tika veikts, izmantojot: 
+        <input type="text" name="" placeholder="(norādīt izmantoto izvērtēšanas metodi; ja izmantotas atšķirīgas metodes, norādīt visas atbilstoši to izmantošanai)" class="iestades-nosaukums" value="<?= $data->method; ?>"></p>
+        <p><br /> Izvērtējumu apliecinošs dokuments: 
+        <input type="text" name="" placeholder="(pievienot hipersaiti uz izvērtējumu apliecinošu dokumentu, protokolu, pārskatu vai citu līdzvērtīgu dokumentu)" class="iestades-nosaukums" value="<?= $data->protocol_url; ?>"></p>
+        <br><br><h2>Atbilstības statuss</h2>
+        <p><em>Atzīmēt vajadzīgo - vienu no Šādiem veidiem</em>:</p>
+
+        <!-- <p>Tīmekļvietne</p><br>
+        <p><input type="checkbox" name="" value="" <?php if ($data->if_fit_status == 1) echo "checked" ?>><strong> pilnīgi atbilst </strong>noteikumiem Nr: 445</p>
+        <p><input type="text"  name="" placeholder="(var norādīt vairākas tīmekļvietnes un/vai mobilās lietotnes)" class="iestades-nosaukums1"></p>
+   
+        <br><p><br /> Tīmekļvietne</p>
+        <p><strong><input type="checkbox" id="" name="" value="" <?php if ($data->if_fit_status == 2) echo "checked" ?>> daļēji atbilst </strong>noteikumiem Nr: 445 šādu neatbilstību/atbrīvojumu dēļ:</p>
+        <p> <input type="text"  name="" placeholder="(var norādīt vairākas tīmekļvietnes un/vai mobilās lietotnes)" class="iestades-nosaukums1"></p>
+        <p><input type="text" name="" placeholder="(norādīt neatbilstošo saturu vai pamatot atbrīvojumu, norādot attiecīgos minēto noteikumu punktus)" class="iestades-nosaukums1"></p><br>
+
+        <p><br />Tīmekļvietne</p>
+        <p><strong><input type="checkbox" name="" value="" <?php if ($data->if_fit_status == 0) echo "checked" ?>> neatbilst </strong>noteikumiem Nr: 445 šādu neatbilstību/atbrīvojumu dēļ:</p>
+        <p> <input type="text" name="" placeholder="(var norādīt vairākas tīmekļvietnes)" class="iestades-nosaukums1"></p>
+
+        <p><input type="text" name="" placeholder="(norādīt neatbilstošo saturu vai pamatot atbrīvojumu, norādot attiecīgos minēto noteikumu punktus)" class="iestades-nosaukums1"></p> -->
+
+        <?= $str; ?>
+
+        <h2>Nepiekļūstamais saturs</h2>
+        <br><p><em>Atzīmēt vajadzīgo - vienu no šādiem veidiem atbilstoši izvēlētajam atbilstības statusam</em>:</p><br>
+        <p><strong><input type="checkbox" name="" value="" <?php if ($data->if_inaccessible_content == 0) echo "checked"; ?>>Neatbilstība prasībām</strong>, kas minētas noteikumos Nr. 445</p>
+        <p><input type="text" name="" placeholder="(norādīt tīmekļvietņu/mobilo lietotņu neatbilstības un/vai norādīt, kuras sadaļas/saturs/funkcijas nav atbilstošas piekļūstamības prasībām)" class="iestades-nosaukums1"></p><br>
+        <p><input type="checkbox" name="" value="" <?php if ($data->if_inaccessible_content == 1) echo "checked"; ?>>Noteikumos Nr. 445 minēto <strong>piekļūstamības prasību nodrošināšana rada nesamērīgu slogu</strong></p>
+        <p><input type="text"  name="" placeholder="(norādīt nepiekļūstamās sadaļas/saturu/funkcijas, uz kurām atbilstoši minēto noteikumu prasībām uz laiku ir attiecināts atbrīvojums nesamērīga sloga dēļ)" class="iestades-nosaukums1"></p><br>
+        <p><strong><input type="checkbox" name="" value="" <?php if ($data->if_inaccessible_content == 2) echo "checked"; ?>>Neattiecas. </strong>Uz saturu neattiecas noteikumu Nr. 445 prasības</p>
+        <p><input type="text" name="" placeholder="(norādīt nepiekļūstamās iedaļas/saturu/funkcijas, uz kurām neattiecas minēto noteikumu prasības)" class="iestades-nosaukums1"></p><br><br>
+        <h2>Piekļūstamības alternatīvas</h2> <div class="pieklustamibas-saturs">(atbilstoši atbilstības statusam un nepiekļūstamajam saturam)</div><br>
+        <p> <input type="text" name="" placeholder="(norādīt piekļūstamības alternatīvas, to saņemšanas iespējas u. c. informāciju atbilstoši alternatīvai)" class="iestades-nosaukums1" value="<?= $data->access_alternative; ?>"></p><br>
+        <h2>Ziņas par paziņojuma sagatavošanu </h2><br>
+        <p>Pirmreizēji sagatavots <input type="text" name="" placeholder="(norādīt pirmreizējā paziņojuma sagatavošanas datumu)" class="iestades-nosaukums" value="<?= $data->note_first_date; ?>"></p><br><br>
+        <p>Atkārtoti pārskatīts <input type="text" name="" placeholder="(norādīt paziņojuma pēdējās pārskatīšanas datumu; nenorāda, ja šis ir pirmreizējais paziņojums)" class="iestades-nosaukums" value="<?= $data->note_remind_date; ?>"></p>
+        <br><br> <h2>Atsauksmēm un saziņai</h2><br><p><input type="text" name="" placeholder="(aprakstīt atsauksmju sniegšanas mehānismu un norādīt hipersaiti, kuru izmantojot iestādi var informēt " class="iestades-nosaukums1"></p> <br>
+        <p><input type="text" name="" placeholder="par atbilstības nepilnībām un pieprasīt piekļūstamo informāciju vai saturu)" class="iestades-nosaukums1"></p><br>
+        <p><input type="text" name="" placeholder="(norādīt kontaktinformāciju saziņai ar iestādi piekļūstamības jautājumos)" class="iestades-nosaukums1"></p><br><br>
+        <h2>Izpildes nodrošināšanas procedūra un sūdzību iesniegšanas kārtība</h2>
+        <p>(aprakstīt izpildes panākšanas procedūru iestādē (iesnieguma vai sūdzības par nepiekļūstamu saturu iesniegšanas un izpildes process) un, ja attiecināms, to<br> pārraugošajā iestādē;</p>
+        <p>norādīt kontaktinformāciju saziņai ar izpildes nodrošinātāju (struktūrvienību))</p>
+        <input type="text" name="" placeholder="" class="iestades-nosaukums1"><br>
+        <p>Ja iestāde, kas atbildīga par attiecīgās tīmekļvietnes vai mobilās lietotnes saturu, nav atbilstoši reaģējusi uz lietotāja iesniegumu vai sūdzību par tīmekļvietnes satura<br> piekļūstamību, lietotājs var iesniegt sūdzību Latvijas Republikas Tiesībsargam:</p><br>
+        <p><input type="text" name="" placeholder="(norādīt kontaktinformāciju saziņai ar Latvijas Republikas Tiesībsargu (hipersaite uz tīmekļvietni un tiesībsarga kontaktinformāciju))" class="iestades-nosaukums1"></p> <br>
+        <div class="footer">Vides aizsardzības un<br>reģionālās attīstības ministrs J. Pūce</div>
+</body>
+</html>
